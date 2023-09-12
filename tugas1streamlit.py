4f15eb1f0bf5b8f0a95f46df12db0c04299f86c0
LittleBabyIcebear <143930653+LittleBabyIcebear@users.noreply.github.com>
2023-09-12 19:19:05 +0700

    Update tugas1streamlit.py

--- tugas1streamlit.py
+++ tugas1streamlit.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import random
+import plotly.express as px
 from Basanitrogen import Codon_DNA
 
 def generate_random_sequence(sequence_type, nitrogen_base, num_sequences):
@@ -30,6 +31,7 @@ def map_codons_to_amino_acids(split_sequence, Codon_DNA):
             if base in data["Codon"]:
                 st.write(f"Codon {base}: {amino_acid} ({data['Single_Letter']})")
 
+
 def main():
     purines = ["A", "G"]
     pyrimidines = ["C", "T", "U"]
@@ -37,7 +39,7 @@ def main():
     st.title("🧬Random DNA/RNA Sequence Generator🧬")
     st.sidebar.title("Choose It! 🔎")
 
-    selected_option = st.sidebar.radio("Select an option", ["Show Amino Acid Table", "Analyze Sequence to Amino Acid"])
+    selected_option = st.sidebar.radio("Select an option", ["Show Amino Acid Table", "Analyze Sequence to Amino Acid", "Analyze Fasta File"])
 
     if selected_option == "Show Amino Acid Table":
         st.subheader("Nitrogen Base Classification")
@@ -48,6 +50,7 @@ def main():
         st.subheader("Related Codon to Amino Acid Table")
         df = pd.DataFrame([(amino_acid, ', '.join(data['Codon']), data['Single_Letter']) for amino_acid, data in Codon_DNA.items()],
                             columns=['Amino Acid', 'Codon', 'Single Letter'])
+
         st.table(df)
 
     elif selected_option == "Analyze Sequence to Amino Acid":
@@ -72,6 +75,66 @@ def main():
 
             st.subheader("Codon to Amino Acid Mapping")
             map_codons_to_amino_acids(split_sequence, Codon_DNA)
+    
+    elif selected_option == "Analyze Fasta File":
+        st.subheader("Uploaded FASTA File")
+        #Elemen upload file
+        uploaded_file = st.file_uploader("Drag FASTA file here!", type=["fasta", "fa"])
+        
+        if st.button("Run"):
+            genome_data_with_header = uploaded_file.read().decode()
+
+            #Deskripsi untuk line pertama  
+            st.subheader("Description of the Genom")
+            lines = genome_data_with_header.split("\n")
+            if lines:
+                desc = lines[0]
+                st.write(desc)
+
+            #Keseluruhan genom 
+            st.subheader("Genome Data:")
+            genome_data_no_header = "".join(genome_data_with_header.split("\n")[1:])  # Remove the first line (header)
+            st.write(genome_data_no_header)
+
+
+            st.subheader("Frequency each Nitrogen Base in The Sequence")
+            Adenin = 0
+            Guanin = 0
+            Cytosin = 0
+            Timin = 0 
+            Error = 0
+            for base in genome_data_no_header:
+                if base == "A":
+                    Adenin+=1
+                elif base == "G":
+                    Guanin+=1
+                elif base == "C":
+                    Cytosin+=1
+                elif base == "T":
+                    Timin+=1
+                else:
+                    Error+= 1
+            
+            st.write(f"Length of the data: {len(genome_data_no_header)}")
+            data = {
+                 "Parameter": ["Adenin", "Guanin", "Cytosin", "Timin", "Error rate"],
+                 "Value (base)": [Adenin, Guanin, Cytosin, Timin, Error / len(genome_data_no_header)]
+             }
+            df = pd.DataFrame(data)
+            st.table(df)
+            
+            st.subheader("Genome Metrics")
+
+            # Create a pie chart for Adenin, Guanin, Timin, and Cytosin using Plotly
+            fig = px.pie(
+                names=['Adenin', 'Guanin', 'Timin', 'Cytosin'],
+                values=[Adenin, Guanin, Timin, Cytosin],
+                title='Genome Metrics: Adenin, Guanin, Timin, and Cytosin'
+            )
+
+            # Display the pie chart using Streamlit
+            st.plotly_chart(fig)
+
 
 if __name__ == "__main__":
     main()
